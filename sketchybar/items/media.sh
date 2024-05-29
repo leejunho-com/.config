24be9a22e506deb264d77446cc99b6c8dd440492
leejunho-com <91662691+leejunho-com@users.noreply.github.com>
2024-05-29 16:43:08 +0900

    Update media.sh

--- sketchybar/items/media.sh
+++ sketchybar/items/media.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 sketchybar --add item media e \
-           --set media label.color=$ACCENT_COLOR \
+           --set media label.color=$WHITE \
                        label.max_chars=20 \
-                       icon.padding_left=0 \
+                       icon.padding_left=10 \
                        scroll_texts=on \
                        icon=􀑪             \
-                       icon.color=$ACCENT_COLOR   \
-                       background.drawing=off \
+                       icon.color=$WHITE   \
+                       background.drawing=on \
+                       background.color=$AERO_0 \
                        script="$PLUGIN_DIR/media.sh" \
            --subscribe media media_change
